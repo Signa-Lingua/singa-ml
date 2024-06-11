dd5f799eceff5b831192619a945a21c5f387164a
ahmad alwi <ahmadalwiam@gmail.com>
2024-06-11 21:11:28 +0700

    update model to train with updated dataset

---
+++ notebooks/utils/rename_file.py
@@ -0,0 +1,30 @@
+import os
+import argparse
+
+
+def bulk_rename_files(folder_path):
+    folder_path = f"./storage/datasets/raw/{folder_path}"
+
+    for filename in os.listdir(folder_path):
+        file_number = int(filename.split(".")[0])
+
+        new_number = file_number + 60
+
+        new_filename = f"{new_number}.avi"
+
+        # get full file paths
+        old_file = os.path.join(folder_path, filename)
+        new_file = os.path.join(folder_path, new_filename)
+
+        # rename the file
+        os.rename(old_file, new_file)
+        print(f"Renamed {filename} to {new_filename}")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "folder", type=str, help="The path to the folder containing files to be renamed"
+)
+args = parser.parse_args()
+
+bulk_rename_files(args.folder)
